567291b7958765d6f1d283896471a964fdb2ebd9
onursteknik <onur.saral@gazi.edu.tr>
2018-03-01 23:00:18 +0300

    Initial commit

---
+++ BurcApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace BurcApi
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+            config.EnableCors();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ BurcApi/Controllers/BurcApiController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace BurcApi.Controllers
+{
+    public class Burclar
+    {
+        public string BurcAdi { get; set; }
+        public string Yorum { get; set; }
+    }
+    public class BurcApiController : ApiController
+    {
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
+        [HttpGet]
+        public List<Burclar> GetBurcs()
+        {
+            string url = "";
+            List<Burclar> burcList= new List<Burclar>();
+            string[] burcDizi = { "koc", "boga", "ikizler", "yengec", "aslan", "basak", "terazi", "akrep", "yay", "oglak", "kova", "balik" };
+            string[] burcIsimler = { "KOÇ", "BOĞA", "İKİZLER", "YENGEÇ", "ASLAN", "BAŞAK", "TERAZİ", "AKREP", "YAY", "OĞLAK", "KOVA", "BALIK" };
+            
+            int i = 0;
+            foreach (string item in burcDizi)
+            {
+                url = "http://www.milliyet.com.tr/ashx/Astroloji.ashx?hCase=BurcDetayPre&tname=burcdetay_"+item+"_1";
+
+                WebRequest req = HttpWebRequest.Create(url);
+                WebResponse res;
+                try
+                {
+                    res = req.GetResponse();
+                    StreamReader data = new StreamReader(res.GetResponseStream());
+                    string icerik = data.ReadToEnd();
+                    int start = icerik.IndexOf("row\">")+5;
+                    int end = icerik.IndexOf("</div")-1;
+                    string h1 = icerik.Substring(start, end - start);
+                    Burclar brc = new Burclar();
+                    brc.BurcAdi = burcIsimler[i];
+                    brc.Yorum = h1;
+                    burcList.Add(brc);
+
+                }
+                catch (Exception ex)
+                {
+                    break;
+                }
+                i++;
+            }
+            return burcList;
+        }
+    }
+}
